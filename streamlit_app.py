682ff854eb10aeaf07110c7349cd2b801e17738c
shahjoyal <132144905+shahjoyal@users.noreply.github.com>
2025-03-08 15:59:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_response(input):
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-2.0-flash')
     response = model.generate_content(input)
     return response.text
 
